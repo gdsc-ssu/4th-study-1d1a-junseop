3f81f5095b143d12e5315a5c84955d252a9a8ed5
macaron <147252523+junseoplee@users.noreply.github.com>
2024-01-03 17:51:37 +0900

    [Bronze V] Title: A×B, Time: 204 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int val1 = scanner.nextInt();
+        int val2 = scanner.nextInt();
+        System.out.println(val1 * val2);
+        }
+    }
