f580b73fa55846ec494341d5373bf54c32fbb182
macaron <147252523+junseoplee@users.noreply.github.com>
2024-01-28 06:19:57 +0900

    [Bronze III] Title: 오븐 시계, Time: 232 ms, Memory: 18556 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int H = scanner.nextInt();
+    int M = scanner.nextInt();
+
+    int additionalMinutes = scanner.nextInt();
+
+    M += additionalMinutes;
+    if (M >= 60) {
+      H += M / 60;
+      M %= 60;
+    }
+    if (H >= 24) {
+      H %= 24;
+    }
+
+    System.out.println(H + " " + M);
+  }
+}
