93371ce32332dff755ed5bd24610d31da6512f4c
macaron <147252523+junseoplee@users.noreply.github.com>
2024-01-30 05:02:08 +0900

    [Bronze IV] Title: 주사위 세개, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int[] dice = new int[3];
+    for (int i = 0; i < 3; i++) {
+      dice[i] = scanner.nextInt();
+    }
+
+    int prize = 0;
+
+    if (dice[0] == dice[1] && dice[1] == dice[2]) {
+      prize = 10000 + dice[0] * 1000;
+    } else if (dice[0] == dice[1] || dice[0] == dice[2]) {
+      prize = 1000 + dice[0] * 100;
+    } else if (dice[1] == dice[2]) {
+      prize = 1000 + dice[1] * 100;
+    } else if (dice[0] != dice[1] || dice[0] != dice[2] || dice[1] != dice[2]) {
+      prize = Math.max(dice[0], Math.max(dice[1], dice[2])) * 100;
+    }
+
+    System.out.println(prize);
+  }
+}
